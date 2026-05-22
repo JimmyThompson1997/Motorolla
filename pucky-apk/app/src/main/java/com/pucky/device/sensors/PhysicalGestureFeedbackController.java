c583662ab8a540a6315f68edd2a6137a5dc8390f
JimmyThompson1997 <jimmythompson323@gmail.com>
2026-05-21 20:12:23 -0700

    Shorten double tap pulses

--- pucky-apk/app/src/main/java/com/pucky/device/sensors/PhysicalGestureFeedbackController.java
+++ pucky-apk/app/src/main/java/com/pucky/device/sensors/PhysicalGestureFeedbackController.java
@@ -54,8 +54,8 @@ public final class PhysicalGestureFeedbackController {
     private static final long CHOP_PING_DELAY_MS = 600L;
     private static final float HINGE_CLOSED_MAX_DEGREES = 20f;
     private static final long DOUBLE_TAP_BUZZ_DELAY_MS = 200L;
-    private static final long DOUBLE_TAP_BUZZ_PULSE_MS = 280L;
-    private static final long DOUBLE_TAP_BUZZ_GAP_MS = 100L;
+    private static final long DOUBLE_TAP_BUZZ_PULSE_MS = 200L;
+    private static final long DOUBLE_TAP_BUZZ_GAP_MS = 250L;
     private static final long DOUBLE_TAP_FEEDBACK_GUARD_MS = 1_650L;
     private static final int MAX_VIBRATION_AMPLITUDE = 255;
     private static final int SENSOR_RATE_US = 10_000;
